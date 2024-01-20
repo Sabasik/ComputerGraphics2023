d54838566842aba28f337a7f7671279713a57167
Henri Sellis <henri.sellis@gmail.com>
2024-01-20 16:42:53 +0200

    Correct number combination destroys wall

---
+++ Assets/Scripts/Activatable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activatable : MonoBehaviour
+{
+    public virtual void Activate()
+    {
+        
+    }
+
+}
--- Assets/Scripts/CheckBox/CheckCombination.cs
+++ Assets/Scripts/CheckBox/CheckCombination.cs
@@ -8,6 +8,11 @@ public class CheckCombination : MonoBehaviour
     public List<GameObject> checkboxes;
     public static CheckCombination checkCombination;
 
+    public int[] combination1 = new int[] {3, 1, 8, 2, 7, 4};
+    public int[] combination2 = new int[] {0, 2, 4, 6, 8};
+    public Activatable activatable1;
+    public Activatable activatable2;
+
     void Awake() {
         checkCombination = this;
     }
@@ -15,22 +20,32 @@ public class CheckCombination : MonoBehaviour
     public void doCheck() {
         if (isFirstCombination()) {
             Debug.Log("first combination");
+            if (activatable1 != null)
+            {
+                activatable1.Activate();
+            }
         }
         if (isSecondCombination()) {
             Debug.Log("second combination");
+            if (activatable2 != null)
+            {
+                activatable2.Activate();
+            }
         }
     }
 
     private bool isFirstCombination() {
-        int[] mustBeChecked = new int[] {1, 5, 6};
+        
         for (int i = 0; i < 9; i++) {
-            if (mustBeChecked.Contains(i)) {
+            if (combination1.Contains(i)) {
                 if (!checkboxes[i].GetComponent<CheckBoxController>().getCheck()) {
+                    Debug.Log(i + " wrong");
                     return false;
                 }
             } else {
                 if (checkboxes[i].GetComponent<CheckBoxController>().getCheck()) {
                     return false;
+                    Debug.Log(i + " wrong");
                 }
             }
         }
@@ -38,9 +53,9 @@ public class CheckCombination : MonoBehaviour
     }
 
     private bool isSecondCombination() {
-        int[] mustBeChecked = new int[] {0, 2, 4, 6, 8};
+        
         for (int i = 0; i < 9; i++) {
-            if (mustBeChecked.Contains(i)) {
+            if (combination2.Contains(i)) {
                 if (!checkboxes[i].GetComponent<CheckBoxController>().getCheck()) {
                     return false;
                 }
